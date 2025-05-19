9a29252335543f7b108409ed0abe97e6386808c2
Vishal Kumar <98923664+ritsvishalcs@users.noreply.github.com>
2025-05-19 21:20:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 var isDate = function (input) {
   //   write your code here
+	function isDate(input) {
+  // Check if it's a Date object and valid
+  if (input instanceof Date && !isNaN(input.getTime())) {
+    return true;
+  }
+
+  // Try to parse string/number into a Date
+  const parsed = new Date(input);
+  return !isNaN(parsed.getTime());
+}
+
 };
 
 // Do not change the code below.
